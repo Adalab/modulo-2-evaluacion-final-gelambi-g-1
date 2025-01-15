4ae4e8eea73d8dc9c0a4ab9f8412671b3b30fc3d
gelambi-g <gelambi.g@gmail.com>
2025-01-15 18:32:40 +0100

    hasta aqui pinta las listas

--- src/js/search.js
+++ src/js/search.js
@@ -1,4 +1,4 @@
-import './listFav';
+
 
 const inputSearch = document.querySelector(".js-input-search");
 const btnSearch = document.querySelector(".js-btn-search");
@@ -6,20 +6,36 @@ const btnSearch = document.querySelector(".js-btn-search");
 let listSeries = [];
 let listSeriesFav = [];
 
+// const ulListFav = document.querySelector(".js-listFav")
+
+// function renderSeriesFav (list){
+//     ulListFav.innerHTML = "";
+//     for (const serieFav of list) {
+//         const findfav = listSeriesFav.find((serieFav) => serieFav.id === listSeries.mal_id)
+
+//         let css = findfav ? "favorite" : "";
+
+//         ulListFav.innerHTML +=
+//         `<li id=${serieFav.mal_id} class="list-li ${css}">
+//             <article>
+//                 <img src="${serieFav.images.jpg.image_url}" alt="">
+//                 <p>${serieFav.title}</p>
+//             </article>
+//         </li>`
+//     }
+// }
+
 function handleClickFav (ev) {
     const liClicked = parseInt (ev.currentTarget.id);
     console.log(liClicked);
     const serieSelected = listSeries.find((eachSerie) => eachSerie.mal_id === liClicked);
-console.log(serieSelected);
 
-    const indexFavSelected = listSeriesFav.filter((serie) => serie.mal_id === liClicked);
-    if(indexFavSelected.length === 0) {
+
+    const indexFavSelected = listSeriesFav.find((serie) => serie.mal_id === liClicked);
+    if(!indexFavSelected) {
         listSeriesFav.push(serieSelected);
-    } else {
-        console.log("jj")
     }
-    
-  renderSeries(listSeries)
+
 }
 
 function listenerSeriesFav () {
@@ -35,21 +51,28 @@ function getDataApi (search) {
     .then((resp) => resp.json())
     .then((data) => {
         console.log(data)
-        listSeries = data.data
-        renderSeries(listSeries)
+        listSeries = data.data;
+        renderSeries(listSeries);
+        // localStorage.setItem("seriesServer" JSON.stringify(listSeries))
     })
 
 };
 
 const ulList = document.querySelector(".js-listResult");
 const boxResult = document.querySelector(".js-box-result");
+const ulListFav = document.querySelector(".js-listFav")
 
 //funcion que pinta la lista de series
 function renderSeries (list) {
     for (const serieList of list) {
+        // const findFav = listSeriesFav.find((serieFav) => serieFav.id === serieList.mal_id);
+        // let css = findFav ? "favorite" : "";
+        //NO SÉ COM FER-HO PERQUÈ SE'M PINTI
+        
+
         const li = document.createElement("li");
         ulList.appendChild(li);
-        li.setAttribute("class", "list-element js-list-element");
+        li.setAttribute("class", `list-element js-list-element`);
         li.setAttribute("id", serieList.mal_id)
 
         const article = document.createElement("article");
@@ -64,11 +87,8 @@ function renderSeries (list) {
         article.append(image, titleP);
 
         if(serieList.images.jpg.image_url === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") {
-        image.setAttribute("src", "https://placehold.co/220x310?text=Imagen+no+disponible")
+        image.setAttribute("src", "https://placehold.co/220x310?text=Imagen+no+disponible");
         };
-
-        listenerSeriesFav();    
-
        
 }};
 
@@ -85,8 +105,12 @@ function handleClickSearch (ev) {
         span.innerHTML = ""
         getDataApi(inputSearchValue)
     }
-    
+    renderSeries(listSeries)  
 }
 
 btnSearch.addEventListener("click", handleClickSearch);
 
+// const dataSeriesLS = localStorage.getItem("seriesServer");
+// if(dataSeriesLS) {
+//     listSeries = JSON.parse(dataSeriesLS)
+// }
